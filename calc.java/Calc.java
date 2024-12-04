791d246cdc2ff10fcfaa9e34303893f22774d5ad
Rosby Roby <rosbyroby07@gmail.com>
2024-12-04 12:15:56 +0530

    hello java file added

--- calc.java/Calc.java
+++ calc.java/Calc.java
@@ -1,19 +1,5 @@
-
-public  class Calc{
-    public static void main(String[] args) {
-        
-    
-     Can obj =new Can();
-     int r= obj.add(5,3);
-     System.out.println(r);
-    }
+public class Calc {
+  public static void main(String[] args) {
+    System.out.println("Hello");
   }
-  an obj =new Can();
-     int r= obj.add(5,3);
-     System.out.println(r);
-    }
-  }
-  an obj =new Can();
-  int r= obj.add(5,3);
-  System.out.println(r);
- }
+}
